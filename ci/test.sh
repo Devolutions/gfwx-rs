9e4af11d46b3340d922d3a8c4c3bb6dc8de3680d
Vlad Alex <vlad.al.dp@gmail.com>
2018-11-13 14:55:21 +0200

    added travis autobuild

---
+++ ci/test.sh
@@ -0,0 +1,20 @@
+set -ex
+
+main() {
+    cross build --target $TARGET --lib
+
+    if [ ! -z $DISABLE_TESTS ]; then
+        return
+    fi
+
+    cross build --target $TARGET --examples
+    cross build --target $TARGET --benches
+
+    cross test --target $TARGET
+    cross test --target $TARGET --release
+}
+
+# we don't run the "test phase" when doing deploys
+if [ -z $TRAVIS_TAG ]; then
+    main
+fi
