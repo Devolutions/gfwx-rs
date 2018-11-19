6ee7d134a61bd630c43069a5c3b42e36ace03175
Vlad Alex <vlad.al.dp@gmail.com>
2018-11-19 21:24:57 +0200

    removed unnecessary bounds check and ptiomized step_by in cubic lifting

--- src/lifting/cubic.rs
+++ src/lifting/cubic.rs
@@ -10,13 +10,13 @@ pub fn lift_cubic(image: &mut [&mut [i16]]) {
         if step < image[0].len() {
             process_maybe_parallel_for_each(
                 image.into_iter().step_by(step),
-                |mut col| horizontal_lift(&mut col, step),
+                |mut col| unsafe { horizontal_lift(&mut col, step) },
                 hint_do_parallel,
             );
         }
 
         if step < image.len() {
-            vertical_lift(image, step);
+            unsafe { vertical_lift(image, step) };
         }
 
         step *= 2;
@@ -34,13 +34,13 @@ pub fn unlift_cubic(image: &mut [&mut [i16]]) {
 
     while step > 0 {
         if step < image.len() {
-            vertical_unlift(image, step);
+            unsafe { vertical_unlift(image, step) };
         }
 
         if step < image[0].len() {
             process_maybe_parallel_for_each(
                 image.into_iter().step_by(step),
-                |mut col| horizontal_unlift(&mut col, step),
+                |mut col| unsafe { horizontal_unlift(&mut col, step) },
                 hint_do_parallel,
             );
         }
@@ -49,136 +49,142 @@ pub fn unlift_cubic(image: &mut [&mut [i16]]) {
     }
 }
 
-fn horizontal_lift(column: &mut [i16], step: usize) {
-    let mut x = step;
-    let mut c0 = column[0];
+unsafe fn horizontal_lift(column: &mut [i16], step: usize) {
+    let mut c0 = *column.get_unchecked(0);
     let mut c1 = c0;
     let mut c2 = if 2 * step < column.len() {
-        column[2 * step]
+        *column.get_unchecked(2 * step)
     } else {
         c0
     };
 
+    let mut x = step;
     if column.len() > 3 * step {
-        for i in (step..column.len() - 3 * step).step_by(step * 2) {
-            let c3 = column[3 * step + i];
-            column[i] = column[i].wrapping_sub(cubic(c0, c1, c2, c3));
+        while x < column.len() - 3 * step {
+            let c3 = *column.get_unchecked(3 * step + x);
+            *column.get_unchecked_mut(x) -= cubic(c0, c1, c2, c3);
             c0 = c1;
             c1 = c2;
             c2 = c3;
-            x = i + step * 2;
+            x += step * 2;
         }
     }
-    for i in (x..column.len()).step_by(step * 2) {
-        column[i] = column[i].wrapping_sub(cubic(c0, c1, c2, c2));
+    while x < column.len() {
+        *column.get_unchecked_mut(x) -= cubic(c0, c1, c2, c2);
         c0 = c1;
         c1 = c2;
+        x += step * 2;
     }
 
-    let mut g0 = column[step];
+    let mut g0 = *column.get_unchecked(step);
     let mut g1 = g0;
     let mut g2 = if 3 * step < column.len() {
-        column[3 * step]
-    } else {
-        g0
-    };
-
-    x = 2 * step;
-    if column.len() > 3 * step {
-        for i in (2 * step..column.len() - 3 * step).step_by(step * 2) {
-            let g3 = column[3 * step + i];
-            column[i] = column[i].wrapping_add(cubic(g0, g1, g2, g3) / 2);
-            g0 = g1;
-            g1 = g2;
-            g2 = g3;
-            x = i + step * 2;
-        }
-    }
-    for i in (x..column.len()).step_by(step * 2) {
-        column[i] = column[i].wrapping_add(cubic(g0, g1, g2, g2) / 2);
-        g0 = g1;
-        g1 = g2;
-    }
-}
-
-fn horizontal_unlift(column: &mut [i16], step: usize) {
-    let mut g0 = column[step];
-    let mut g1 = g0;
-    let mut g2 = if 3 * step < column.len() {
-        column[3 * step]
+        *column.get_unchecked(3 * step)
     } else {
         g0
     };
 
     let mut x = 2 * step;
     if column.len() > 3 * step {
-        for i in (2 * step..column.len() - 3 * step).step_by(step * 2) {
-            let g3 = column[3 * step + i];
-            column[i] = column[i].wrapping_sub(cubic(g0, g1, g2, g3) / 2);
+        while x < column.len() - 3 * step {
+            let g3 = *column.get_unchecked(3 * step + x);
+            *column.get_unchecked_mut(x) += cubic(g0, g1, g2, g3) / 2;
             g0 = g1;
             g1 = g2;
             g2 = g3;
-            x = i + step * 2;
+            x += step * 2;
         }
     }
-    for i in (x..column.len()).step_by(step * 2) {
-        column[i] = column[i].wrapping_sub(cubic(g0, g1, g2, g2) / 2);
+    while x < column.len() {
+        *column.get_unchecked_mut(x) += cubic(g0, g1, g2, g2) / 2;
         g0 = g1;
         g1 = g2;
+        x += step * 2;
+    }
+}
+
+unsafe fn horizontal_unlift(column: &mut [i16], step: usize) {
+    let mut g0 = *column.get_unchecked(step);
+    let mut g1 = g0;
+    let mut g2 = if 3 * step < column.len() {
+        *column.get_unchecked(3 * step)
+    } else {
+        g0
+    };
+
+    let mut x = 2 * step;
+    if column.len() > 3 * step {
+        while x < column.len() - 3 * step {
+            let g3 = *column.get_unchecked(3 * step + x);
+            *column.get_unchecked_mut(x) -= cubic(g0, g1, g2, g3) / 2;
+            g0 = g1;
+            g1 = g2;
+            g2 = g3;
+            x += step * 2;
+        }
+    }
+    while x < column.len() {
+        *column.get_unchecked_mut(x) -= cubic(g0, g1, g2, g2) / 2;
+        g0 = g1;
+        g1 = g2;
+        x += step * 2;
     }
 
-    let mut c0 = column[0];
+    let mut c0 = *column.get_unchecked(0);
     let mut c1 = c0;
     let mut c2 = if 2 * step < column.len() {
-        column[2 * step]
+        *column.get_unchecked(2 * step)
     } else {
         c0
     };
 
-    x = step;
+    let mut x = step;
     if column.len() > 3 * step {
-        for i in (step..column.len() - 3 * step).step_by(step * 2) {
-            let c3 = column[3 * step + i];
-            column[i] = column[i].wrapping_add(cubic(c0, c1, c2, c3));
+        while x < column.len() - 3 * step {
+            let c3 = *column.get_unchecked(3 * step + x);
+            *column.get_unchecked_mut(x) += cubic(c0, c1, c2, c3);
             c0 = c1;
             c1 = c2;
             c2 = c3;
-            x = i + step * 2;
+            x += step * 2;
         }
     }
-    for i in (x..column.len()).step_by(step * 2) {
-        column[i] = column[i].wrapping_add(cubic(c0, c1, c2, c2));
+    while x < column.len() {
+        *column.get_unchecked_mut(x) += cubic(c0, c1, c2, c2);
         c0 = c1;
         c1 = c2;
+        x += step * 2;
     }
 }
 
-fn vertical_lift(mut image: &mut [&mut [i16]], step: usize) {
+unsafe fn vertical_lift(mut image: &mut [&mut [i16]], step: usize) {
     let hint_do_parallel =
         image.len() * image[0].len() > Config::multithreading_factors().cubic_vertical_lifting;
 
     process_maybe_parallel_for_each(
         DoubleOverlappingChunksIterator::from_slice(&mut image, step),
         |(prev_left, left, middle, right, next_right)| {
-            for x in (0..middle[0].len()).step_by(step) {
-                let c1 = left[0][x];
+            let mut x = 0;
+            while x < middle.get_unchecked(0).len() {
+                let c1 = *left.get_unchecked(0).get_unchecked(x);
                 let c2 = if let Some(right_value) = right.first() {
-                    right_value[x]
+                    *right_value.get_unchecked(x)
                 } else {
                     c1
                 };
                 let c0 = if let Some(prev_left_value) = prev_left.first() {
-                    prev_left_value[x]
+                    *prev_left_value.get_unchecked(x)
                 } else {
                     c1
                 };
                 let c3 = if let Some(next_right_value) = next_right.first() {
-                    next_right_value[x]
+                    *next_right_value.get_unchecked(x)
                 } else {
                     c2
                 };
 
-                middle[0][x] = middle[0][x].wrapping_sub(cubic(c0, c1, c2, c3));
+                *middle.get_unchecked_mut(0).get_unchecked_mut(x) -= cubic(c0, c1, c2, c3);
+                x += step;
             }
         },
         hint_do_parallel,
@@ -187,57 +193,61 @@ fn vertical_lift(mut image: &mut [&mut [i16]], step: usize) {
     process_maybe_parallel_for_each(
         DoubleOverlappingChunksIterator::from_slice(&mut image[step..], step),
         |(prev_left, left, middle, right, next_right)| {
-            for x in (0..middle[0].len()).step_by(step) {
-                let g1 = left[0][x];
+            let mut x = 0;
+            while x < middle.get_unchecked(0).len() {
+                let g1 = *left.get_unchecked(0).get_unchecked(x);
                 let g2 = if let Some(right_value) = right.first() {
-                    right_value[x]
+                    *right_value.get_unchecked(x)
                 } else {
                     g1
                 };
                 let g0 = if let Some(prev_left_value) = prev_left.first() {
-                    prev_left_value[x]
+                    *prev_left_value.get_unchecked(x)
                 } else {
                     g1
                 };
                 let g3 = if let Some(next_right_value) = next_right.first() {
-                    next_right_value[x]
+                    *next_right_value.get_unchecked(x)
                 } else {
                     g2
                 };
 
-                middle[0][x] = middle[0][x].wrapping_add(cubic(g0, g1, g2, g3) / 2);
+                *middle.get_unchecked_mut(0).get_unchecked_mut(x) += cubic(g0, g1, g2, g3) / 2;
+                x += step;
             }
         },
         hint_do_parallel,
     );
 }
 
-fn vertical_unlift(mut image: &mut [&mut [i16]], step: usize) {
+unsafe fn vertical_unlift(mut image: &mut [&mut [i16]], step: usize) {
     let hint_do_parallel =
         image.len() * image[0].len() > Config::multithreading_factors().cubic_vertical_lifting;
 
     process_maybe_parallel_for_each(
         DoubleOverlappingChunksIterator::from_slice(&mut image[step..], step),
         |(prev_left, left, middle, right, next_right)| {
-            for x in (0..middle[0].len()).step_by(step) {
-                let g1 = left[0][x];
+            let mut x = 0;
+            while x < middle.get_unchecked(0).len() {
+                let g1 = *left.get_unchecked(0).get_unchecked(x);
                 let g2 = if let Some(right_value) = right.first() {
-                    right_value[x]
+                    *right_value.get_unchecked(x)
                 } else {
                     g1
                 };
                 let g0 = if let Some(prev_left_value) = prev_left.first() {
-                    prev_left_value[x]
+                    *prev_left_value.get_unchecked(x)
                 } else {
                     g1
                 };
                 let g3 = if let Some(next_right_value) = next_right.first() {
-                    next_right_value[x]
+                    *next_right_value.get_unchecked(x)
                 } else {
                     g2
                 };
 
-                middle[0][x] = middle[0][x].wrapping_sub(cubic(g0, g1, g2, g3) / 2);
+                *middle.get_unchecked_mut(0).get_unchecked_mut(x) -= cubic(g0, g1, g2, g3) / 2;
+                x += step;
             }
         },
         hint_do_parallel,
@@ -246,25 +256,27 @@ fn vertical_unlift(mut image: &mut [&mut [i16]], step: usize) {
     process_maybe_parallel_for_each(
         DoubleOverlappingChunksIterator::from_slice(&mut image, step),
         |(prev_left, left, middle, right, next_right)| {
-            for x in (0..middle[0].len()).step_by(step) {
-                let c1 = left[0][x];
+            let mut x = 0;
+            while x < middle.get_unchecked(0).len() {
+                let c1 = *left.get_unchecked(0).get_unchecked(x);
                 let c2 = if let Some(right_value) = right.first() {
-                    right_value[x]
+                    *right_value.get_unchecked(x)
                 } else {
                     c1
                 };
                 let c0 = if let Some(prev_left_value) = prev_left.first() {
-                    prev_left_value[x]
+                    *prev_left_value.get_unchecked(x)
                 } else {
                     c1
                 };
                 let c3 = if let Some(next_right_value) = next_right.first() {
-                    next_right_value[x]
+                    *next_right_value.get_unchecked(x)
                 } else {
                     c2
                 };
 
-                middle[0][x] = middle[0][x].wrapping_add(cubic(c0, c1, c2, c3));
+                *middle.get_unchecked_mut(0).get_unchecked_mut(x) += cubic(c0, c1, c2, c3);
+                x += step;
             }
         },
         hint_do_parallel,
